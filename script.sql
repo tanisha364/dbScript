533ec0c802e4f00f8ae0948e81b4608a016f06a2
Tanisha Garg <tanishagarg364@gmail.com>
2024-02-06 03:03:10 -0500

    add relation between order and product table

--- script.sql
+++ script.sql
@@ -28,3 +28,13 @@ CREATE TABLE IF NOT EXISTS products (
     description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Create the 'order_items' table
+CREATE TABLE IF NOT EXISTS order_items (
+    order_item_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
